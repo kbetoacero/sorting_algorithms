03bd18515748c4d7f583d0634212b9afaab5df3a
kbetoacero <2028@holbertonschool.com>
2020-10-19 16:53:43 +0000

    0-O, 0-bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * swaparr - swaparr function
+ *@a : int
+ *@b : int
+ * Return: void
+ */
+
+void swap(int *a, int *b)
+{
+	int tmp = 0;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
+
+/**
+ * bubble_sort - bubble_sort function
+ *@array : array
+ *@size : size
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j;
+
+	while (size >= 2 && i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
